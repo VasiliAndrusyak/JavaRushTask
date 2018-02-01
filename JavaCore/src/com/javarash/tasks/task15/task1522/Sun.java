46b964d5d19ec17fc939a0e1a8990eb4814ce563
VasiliAndrusyak <sky7sky7@gmail.com>
2018-02-01 19:38:46 +0300

    Create Sun.java

---
+++ JavaCore/src/com/javarash/tasks/task15/task1522/Sun.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1522;
+
+public class Sun implements Planet{
+
+    private static Sun instance;
+
+    public static Sun getInstance() {
+        if (instance == null) {
+            instance =  new Sun();
+        }
+        return instance;
+    }
+
+
+    private Sun() {
+    }
+}
