01480c35de8ab932d6a718a804634aa6a7fbf119
VasiliAndrusyak <sky7sky7@gmail.com>
2018-02-01 20:20:23 +0300

    Create Solution.java

---
+++ JavaCore/src/com/javarash/tasks/task15/task1523/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public  Solution(int x) {
+    }
+    protected   Solution(int x,int y) {
+    }
+     Solution(String s) {
+    }
+
+    private Solution(double q) {
+
+    }
+
+}
