e955d0858cf0c17ac720a68ae558334a0f43d175
VasiliAndrusyak <sky7sky7@gmail.com>
2018-02-01 20:21:11 +0300

    Create SubSolution.java

---
+++ JavaCore/src/com/javarash/tasks/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int x) {
+        super(x);
+    }
+
+    protected SubSolution(int x, int y) {
+        super(x, y);
+    }
+
+     SubSolution(String s) {
+        super(s);
+    }
+}
