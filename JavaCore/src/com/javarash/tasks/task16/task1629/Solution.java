0f9d2089feb2c279c303ddf891973f69da0a82fd
VasiliAndrusyak <sky7sky7@gmail.com>
2018-05-02 14:34:07 +0300

    Update Solution.java

--- JavaCore/src/com/javarash/tasks/task16/task1629/Solution.java
+++ JavaCore/src/com/javarash/tasks/task16/task1629/Solution.java
@@ -8,59 +8,49 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
-    public static volatile AtomicInteger countReadStrings = new AtomicInteger(0);
+  //  public static volatile AtomicInteger countReadStrings = new AtomicInteger(0);
     public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
 
     public static void main(String[] args) throws InterruptedException, IOException {
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
 
-        int count = Integer.parseInt(reader.readLine());
-
         //add your code here - добавьте код тут
         t1.start();
-        t2.start();
         t1.join();
+        t2.start();
         t2.join();
 
         t1.printResult();
         t2.printResult();
 
-        while (count > countReadStrings.get()) {
-            }
-
-            t1.interrupted();
-            t2.interrupted();
-            reader.close();
-
     }
 
     //add your code here - добавьте код тут
-    public static class Read3Strings extends Thread {
-        private List<String> result = new ArrayList<String>();
+public static class Read3Strings extends Thread  {
 
-        public void printResult(){
-            System.out.println(result);
-        }
+        private List<String> result = new ArrayList<String>();
 
         @Override
         public void run() {
-
-            while (!isInterrupted()) {
-                try {
+          int count = 0;
+            while (count < 3) {
+            try {
                     if (reader.ready()) {
                         result.add(reader.readLine());
-                        countReadStrings.addAndGet(1);
+                        count++;
                     }
-
-                } catch (IOException e) {
+                } catch(IOException e){
                     e.printStackTrace();
                 }
             }
+       }
 
+        public void printResult(){
+
+            String s = result.get(0)+ " ".concat(result.get(1)+ " ".concat(result.get(2)+ " "));
+            System.out.println(s);
         }
-
         @Override
         public String toString() {
             return result.toString();
