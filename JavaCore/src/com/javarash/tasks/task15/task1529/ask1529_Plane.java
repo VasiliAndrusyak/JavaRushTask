c6b4b1be0e6cd89b4a42c374b5e44740cbfc0155
VasiliAndrusyak <sky7sky7@gmail.com>
2018-05-15 21:40:23 +0300

    Create ask1529_Plane.java

---
+++ JavaCore/src/com/javarash/tasks/task15/task1529/ask1529_Plane.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1529;
+
+public  class Plane implements Flyable {
+
+    int count;
+
+    public Plane(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
